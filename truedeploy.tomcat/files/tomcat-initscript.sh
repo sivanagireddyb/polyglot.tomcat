1af1ec157dec4c90e8487d7c47d365e90ecbeb66
sivanagireddyb <sivanagireddy208@gmail.com>
2017-10-27 13:05:50 +0530

    roles

--- truedeploy.tomcat/files/tomcat-initscript.sh
+++ truedeploy.tomcat/files/tomcat-initscript.sh
@@ -11,15 +11,15 @@ Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
 Environment=CATALINA_HOME=/opt/tomcat
 Environment=CATALINA_BASE=/opt/tomcat
 Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=prd'
 
 ExecStart=/opt/tomcat/bin/startup.sh
 ExecStop=/bin/kill -15 $MAINPID
 
 User=tomcat
 Group=tomcat
-UMask=0007
-RestartSec=10
+#UMask=0007
+#RestartSec=10
 Restart=always
 
 [Install]
